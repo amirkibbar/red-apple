879b44ee34ec57507e185c899f34147c84b3b831
Amir Kibbar <amir.kibbar@hp.com>
2015-05-15 16:10:12 -0700

    first working version - for now it can only start, can't stop consul, and even that - only on windows

---
+++ src/main/groovy/ajk/gradle/ConsulExtension.groovy
@@ -0,0 +1,8 @@
+package ajk.gradle
+
+class ConsulExtension {
+    String version
+    int httpPort
+    int dnsPort
+    File consulDir
+}
---
+++ src/main/groovy/ajk/gradle/ConsulPlugin.groovy
@@ -0,0 +1,38 @@
+package ajk.gradle
+
+import org.gradle.BuildAdapter
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.invocation.Gradle
+
+class ConsulPlugin implements Plugin<Project> {
+    private Project project
+
+    @Override
+    void apply(Project project) {
+        this.project = project
+
+        StartConsulTask startConsul = project.task(type: StartConsulTask, 'startConsul')
+        def extension = project.extensions.create('consul', ConsulExtension)
+        extension.with {
+            version = StartConsulTask.DEFAULT_VERSION
+            httpPort = StartConsulTask.DEFAULT_HTTP_PORT
+            dnsPort = StartConsulTask.DEFAULT_DNS_PORT
+            consulDir = new File(project.rootProject.projectDir.absolutePath + "/gradle/tools/consul")
+        }
+
+        def projectAdapter = [
+                startConsul      : startConsul,
+                projectsEvaluated: { Gradle gradle ->
+                    startConsul.with {
+                        version = extension.version
+                        httpPort = extension.httpPort
+                        dnsPort = extension.dnsPort
+                        consulDir = extension.consulDir
+                    }
+                }
+        ] as BuildAdapter
+
+        project.gradle.addBuildListener(projectAdapter)
+    }
+}
